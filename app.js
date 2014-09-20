bc9b6ec5d29cf34ba64d5a20b95993130ed5c3a7
Maximillian von Briesen <mobyvb@gmail.com>
2014-09-19 23:13:04 -0400

    add styles

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var MongoClient = require('mongodb').MongoClient;
 var session = require('express-session');
+var stylus = require('stylus');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
+app.use(stylus.middleware({src: __dirname + '/public/styles/'}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.listen(3000);
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('landing');
 });
