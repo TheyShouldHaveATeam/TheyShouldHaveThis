f4e2ffc6b5b7dc4df90f83dcd22faa516d1160e7
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2014-09-20 00:27:56 -0400

    createUser() functional

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var MongoClient = require('mongodb').MongoClient;
 var session = require('express-session');
 var stylus = require('stylus');
 
+var dbUser = require(__dirname + '/db/user.js');
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
@@ -17,12 +19,17 @@ app.use(bodyParser.json());
 app.listen(3000);
 
 MongoClient.connect((process.env.MONGODB_CONNECT
-        || "mongodb://localhost:27017/TSHTTest"), function(err, db) {
+        || "mongodb://localhost:27017/TSHT"), function(err, db) {
     if(err) {
         throw err;
     }
 
     app.get('/', function(req, res) {
+
+        // dbUser.createUser(db, "Rapha", "rapha@email.com", "myPassword", function(user) {
+        //     console.log(user);
+        // });
+
         res.render('landing');
     });
 });
