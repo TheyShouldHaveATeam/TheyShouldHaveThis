6b8f374b005c7c31a2003e139c791f7b9753f584
Aniruddha Nadkarni <aniruddha.nadkarni.1996@gmail.com>
2014-09-19 23:57:20 -0400

    Beginning of session work

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var MongoClient = require('mongodb').MongoClient;
-var session = require('session');
+var jwt = require('express-jwt');
+
 
 app.set('views', '/views');
 app.set('view engine', 'jade');
 
 app.use(express.static("public"));
-app.user(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.listen(3000);
+
+app.get('/protected',
+  jwt({secret: 'shhhhhhared-secret'}),
+  function(req, res) {
+    if (!req.user.admin) return res.send(401);
+    res.send(200);
+  });
+
 
 app.get('/', function(req, res) {
     res.render('index');
