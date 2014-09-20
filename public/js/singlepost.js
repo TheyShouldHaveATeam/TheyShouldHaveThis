c1d55cb73ea0ccdd189b5c3a8748b4a6642c63b9
Maximillian von Briesen <mobyvb@gmail.com>
2014-09-20 19:56:55 -0400

    continue work on single post and post list

--- public/js/singlepost.js
+++ public/js/singlepost.js
@@ -1,10 +1,14 @@
 /** @jsx React.DOM */
 
 var SinglePost = React.createClass( {
-
     getInitialState: function() {
         return {
-            post:
+            idea: '',
+            desc: '',
+            category: '',
+            createdOn: 0,
+            votes: {upvotes:0, downvotes:0},
+            comments: {theyHave:0, canMake:0, comment:0}
         };
     },
 
@@ -13,10 +17,17 @@ var SinglePost = React.createClass( {
 
         $.ajax({
             type: 'GET',
-            url: '/posts/'+this.props.postId,
+            url: '/posts/'+this.props.postId+'.json',
             success: function(post) {
                 console.log(post);
-                self.setState({post: post});
+                self.setState({
+                    idea: post.idea,
+                    desc: post.desc,
+                    category: post.category,
+                    createdOn: post.createdOn,
+                    votes: post.votes,
+                    comments: post.comments
+                });
             },
             error: function(error) {
                 console.log('error getting post');
@@ -26,17 +37,17 @@ var SinglePost = React.createClass( {
     },
 
     render: function() {
-        console.log(JSON.stringify(this.state);
         return (
-            <h1>test</h1>
-
+            <div className='single-post'>
+                <h1>{this.state.idea}</h1>
+                <p>{this.state.desc}</p>
+            </div>
         );
 
     }
 });
 
 var postId = $("#current-post").html();
-
 React.renderComponent(
     <SinglePost postId = {postId} />,
     document.getElementById('single-post')
